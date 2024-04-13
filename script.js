cb8aaed318b3d5918564965ce7602481c5ced789
AlexanderMarriott <a1exviper@icloud.com>
2024-04-13 15:58:19 +0100

    added button to play sounds to ensure they are referenced correctly

--- script.js
+++ script.js
@@ -84,14 +84,14 @@ function createPairs(pairs) {
     "fa-cat": "assets/audio/cat.mp3",
     "fa-crow": "assets/audio/crow.mp3",
     "fa-dove": "assets/audio/dove.mp3",
-    "fa-dragon": "assets/audio/dragon.mp3",
-    "fa-horse": "assets/audio/horse.mp3",
-    "fa-hippo": "assets/audio/hippo.mp3",
-    "fa-fish": "assets/audio/fish.mp3",
-    "fa-frog": "assets/audio/frog.mp3",
-    "fa-kiwi-bird": "assets/audio/kiwi.mp3",
-    "fa-otter": "assets/audio/otter.mp3",
-    "fa-spider": "assets/audio/spider.mp3",
+    "fa-dragon": "sounds/dragon.mp3",
+    "fa-horse": "sounds/horse.mp3",
+    "fa-hippo": "sounds/hippo.mp3",
+    "fa-fish": "sounds/fish.mp3",
+    "fa-frog": "sounds/frog.mp3",
+    "fa-kiwi-bird": "sounds/kiwi.mp3",
+    "fa-otter": "sounds/otter.mp3",
+    "fa-spider": "sounds/spider.mp3",
     // ... add more icons and sounds as needed ...
   };
 
@@ -223,4 +223,36 @@ difficultyButtons.forEach((button) => {
     // Start the game
     createPairs(pairs);
   });
+});
+
+let iconSounds = {
+  "fa-dog": "assets/audio/dog.mp3",
+  "fa-cat": "assets/audio/cat.mp3",
+  "fa-crow": "assets/audio/crow.mp3",
+  "fa-dove": "assets/audio/dove.mp3",
+  "fa-dragon": "assets/audio/dragon.mp3",
+  "fa-horse": "assets/audio/horse.mp3",
+  "fa-hippo": "assets/audio/hippo.mp3",
+  "fa-fish": "assets/audio/fish.mp3",
+  "fa-frog": "assets/audio/frog.mp3",
+  "fa-kiwi-bird": "assets/audio/kiwi.mp3",
+  "fa-otter": "assets/audio/otter.mp3",
+  "fa-spider": "assets/audio/spider.mp3",
+  // ... add more icons and sounds as needed ...
+};
+
+let playSoundsButton = document.getElementById("play-sounds");
+let soundIndex = 0;
+let soundKeys = Object.keys(iconSounds);
+
+playSoundsButton.addEventListener("click", function () {
+  // Create a new audio element and play the sound
+  let soundFile = iconSounds[soundKeys[soundIndex]];
+  if (soundFile) {
+    let audio = new Audio(soundFile);
+    audio.play();
+  }
+
+  // Move to the next sound file for the next click
+  soundIndex = (soundIndex + 1) % soundKeys.length;
 });
